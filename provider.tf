b59e580a989f48dfe7fdf36670f81fe829c03b8b
SomyaGoyal <115345500+goyal2@users.noreply.github.com>
2025-04-04 13:04:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.93.0"
+      version = ">=3.0.0"
     }
   }
+required_version = ">=1.0.0"
 }
 provider "azurerm" {
 
